096610ddd70fb854fc6ffd0ccad4bef15a354d42
Николай Ильницкий <kolyailnitsk@Nikolajs-MacBook-Air.local>
2024-03-09 17:39:17 +0300

    the final styles of the group

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -4,15 +4,15 @@ import {IGroup} from "../../types/Groups";
 const Group = ({group} : {group: IGroup}) => {
     return (
         <div className={styles.group__wrapper}>
+            <div style={{backgroundColor:group.avatar_color,borderRadius:"50%",width:"100px",height:"100px",marginBlock:'auto',marginLeft:10}}></div>
             <div className={styles.main_info}>
                 <div style={{fontSize:17,fontWeight:600}}>{group.name}</div>
-                <div style={{color:"#8c8c8c",display:'flex',justifyContent:'space-between'}}>
+                <div className={styles.types__group}>
                     {group.closed? <span>Тип приватности: закрытая</span> : <span>Тип приватности: открытая</span>}
                     {group.members_count? <span>Подписчики: {group.members_count}</span> : null}
                     {group.friends? <span>Подписано друзей: {group.friends.length}</span> : null}
                 </div>
             </div>
-            <div style={{backgroundColor:group.avatar_color,borderRadius:"50%",width:"3rem",height:"3rem",marginBlock:'auto',marginRight:10}}></div>
         </div>
     );
 };
